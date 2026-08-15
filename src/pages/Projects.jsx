4d2997f733ea12f03ed64cce0adb7d2d66df9dc1
Abdul Raziq <abdulrazeq349@gmail.com>
2026-08-16 01:05:22 +0430

    Update Projects.jsx

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -28,9 +28,19 @@ const projects = [
     description:
       "My personal portfolio built with React and Bootstrap.",
     technology: "React • Bootstrap • CSS",
-    image: "/portfolio.jpg",
+    image:process.env.PUBLIC_URL"/portfolio.jpg",
     github: "https://ab-raziq.github.io/portfolio/"
   },
+
+  
+  {
+    id: 4,
+    title: "Keller Website",
+    description: "A modern and responsive job search platform built with HTML, CSS and JavaScript, designed to help users discover and explore job opportunities with ease.",
+    technology: "React • Bootstrap • CSS",
+    image:process.env.PUBLIC_URL"keller.jpg",
+    github: "https://ab-raziq.github.io/MY-Project-Html-Css/keller/"
+  
 ];
 
 const Projects = () => {
