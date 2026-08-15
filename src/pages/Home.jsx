324e0377252519e0e0d67989d0652ab431611e97
Abdul Raziq <abdulrazeq349@gmail.com>
2026-08-16 01:10:07 +0430

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,33 +1,45 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 import { ThemeContext } from "../context/ThemeContext";
 
 const Home = () => {
-    const {darkMode} = useContext(ThemeContext)
+  const { darkMode } = useContext(ThemeContext);
+  const navigate = useNavigate();
+
   return (
     <>
-        <div className={`container-fluid container-hero ${darkMode ? "home dark" : "home"}`}>
-            <div className="inner-hero">
-            <h1>Hi! I'm Abdul Raziq</h1>
+      <div
+        className={`container-fluid container-hero ${
+          darkMode ? "home dark" : "home"
+        }`}
+      >
+        <div className="inner-hero">
+          <h1>Hi! I'm Abdul Raziq</h1>
 
-                <h2>React Developer</h2>
+          <h2>React Developer</h2>
 
-                <p>
-                    Ready to build modern and responsive web applications.
-                </p>
+          <p>
+            Ready to build modern and responsive web applications.
+          </p>
 
-                <button className="btn btn-success">
-                    View My Projects
-                </button>
-            </div>
-            <div className="image">
-                 <img
-                    src={`${process.env.PUBLIC_URL}/image.jpg`}
-                    alt="Abdul Raziq"
-                />
-            </div>
+          <button
+            type="button"
+            className="btn btn-success"
+            onClick={() => navigate("/projects")}
+          >
+            View My Projects
+          </button>
         </div>
+
+        <div className="image">
+          <img
+            src={`${process.env.PUBLIC_URL}/image.jpg`}
+            alt="Abdul Raziq"
+          />
+        </div>
+      </div>
     </>
-  )
+  );
 };
 
 export default Home;
