d48f9d65270049af09b1248898d695c8ce8ae49f
Abdul Raziq <abdulrazeq349@gmail.com>
2026-08-15 22:51:36 +0430

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Navbar from "./components/Navbar";
-import {BrowserRouter as Router , Routes ,Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./pages/Home";
